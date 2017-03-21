a590a0797ed6bf689f16375d47149cf9f2f19582
Antonio Lassandro <lassandroan@gmail.com>
2017-03-21 17:39:44 -0500

    Added thumbArea.getX() Range Checking To Prevent Thumb Out-Of-Bounds For HackAudio::Button::SlidingToggle

--- components/hack_audio_Button.cpp
+++ components/hack_audio_Button.cpp
@@ -207,7 +207,7 @@ void HackAudio::Button::timerCallback()
         thumbArea.translate(animationVel, 0);
         indicatorArea.setWidth((thumbArea.getX() - indicatorArea.getX()) + thumbArea.getWidth()/2);
 
-        if (thumbArea.getPosition().getDistanceFrom(animationEnd) < 4)
+        if (thumbArea.getPosition().getDistanceFrom(animationEnd) <= 4 || thumbArea.getX() < 32 || thumbArea.getX() > 64)
         {
             animationAcc = 0;
             animationVel = 0;
