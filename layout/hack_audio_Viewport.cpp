c29942aebb2798a6e58e53d66893be21a80e4831
Antonio Lassandro <lassandroan@gmail.com>
2017-03-25 01:09:49 -0500

    Draw Connection Nodes Over In/Out Of Diagram; Repainting/Resizing Fixes

--- layout/hack_audio_Viewport.cpp
+++ layout/hack_audio_Viewport.cpp
@@ -18,6 +18,8 @@ void HackAudio::Viewport::setViewedComponent(juce::Component *c)
 {
     contentContainer.removeAllChildren();
     contentContainer.addAndMakeVisible(c);
+    resized();
+    repaint();
 }
 
 void HackAudio::Viewport::mouseDown(const juce::MouseEvent& e)
@@ -52,13 +54,17 @@ void HackAudio::Viewport::mouseWheelMove(const juce::MouseEvent& e, const juce::
     pos.x -= w.deltaX * 128;
     pos.y -= w.deltaY * 128;
     contentContainer.getChildComponent(0)->setTopLeftPosition(pos);
+
+    repaint();
     
 }
 
 void HackAudio::Viewport::paint(juce::Graphics& g)
 {
+
     g.setColour(HackAudio::Colours::Black);
     g.fillRect(contentContainer.getBounds());
+
 }
 
 void HackAudio::Viewport::paintOverChildren(juce::Graphics& g)
@@ -78,14 +84,28 @@ void HackAudio::Viewport::paintOverChildren(juce::Graphics& g)
     int x4 = contentContainer.getX() + contentContainer.getWidth();
     int y4 = contentContainer.getY() + getHeight() / 2;
 
+    g.setColour(HackAudio::Colours::Gray);
+    g.fillEllipse(x2, y2 - 8, 16, 16);
+    g.setColour(HackAudio::Colours::Black);
+    g.drawEllipse(x2, y2 - 8, 16, 16, 4);
+
+    g.setColour(HackAudio::Colours::Gray);
+    g.fillEllipse(x3, y3 - 8, 16, 16);
+    g.setColour(HackAudio::Colours::Black);
+    g.drawEllipse(x3, y3 - 8, 16, 16, 4);
+
     juce::Path p;
     p.startNewSubPath(x1, y1);
-    p.cubicTo(x2, y1, x1, y2, x2, y2);
-    p.startNewSubPath(x3, y3);
+    p.cubicTo(x2, y1, x1, y2, x2 + 4, y2);
+    p.startNewSubPath(x3 + 4, y3);
     p.cubicTo(x4, y3, x3, y4, x4, y4);
 
     g.setColour(HackAudio::Colours::Gray);
     g.strokePath(p, juce::PathStrokeType(4));
+
+    g.setColour(HackAudio::Colours::Gray);
+    g.fillRect(0, 0, 8, getHeight());
+    g.fillRect(getWidth() - 8, 0, 8, getHeight());
 
     g.setColour(HackAudio::Colours::Black);
     g.fillEllipse(0, (getHeight() / 2) - 8, 16, 16);
@@ -112,5 +132,10 @@ void HackAudio::Viewport::resized()
     resizeGuard = false;
 
     contentContainer.centreWithSize(width, height);
+
+    if(juce::Component* c = contentContainer.getChildComponent(0))
+    {
+        c->centreWithSize(c->getWidth(), c->getHeight());
+    }
 
 }
