f0c0aed1bc31fdc959928ad7ea7cf41bdf7da44f
Antonio Lassandro <lassandroan@gmail.com>
2017-04-28 15:07:42 -0500

    Naming Consistency & Formatting Correction

--- diagrams/filters/hack_audio_LPCombFilter.h
+++ diagrams/filters/hack_audio_LPCombFilter.h
@@ -4,57 +4,57 @@
 struct LPCombFilter : public HackAudio::Diagram
 {
     
-    HackAudio::Label delay1;
-    HackAudio::Label fb_gain1;
-    HackAudio::Diagram::Junction sum1;
-    HackAudio::Diagram::Junction mult1;
-    HackAudio::Diagram::Junction fb_node1;
+    HackAudio::Label delay_1;
+    HackAudio::Label fb_1_gain;
+    HackAudio::Diagram::Junction sum_1;
+    HackAudio::Diagram::Junction mult_1;
+    HackAudio::Diagram::Junction fb_1_node;
     
-    HackAudio::Diagram::Junction sum2;
-    HackAudio::Diagram::Junction mult2;
-    HackAudio::Label delay2;
+    HackAudio::Diagram::Junction sum_2;
+    HackAudio::Diagram::Junction mult_2;
+    HackAudio::Label delay_2;
     HackAudio::Label fb_gain2;
-    HackAudio::Diagram::Junction fb_node2;
+    HackAudio::Diagram::Junction fb_2_node;
 
     LPCombFilter()
     {
         
-        sum1.setSymbol(HackAudio::Diagram::Junction::Symbol::Add);
-        sum1.setBounds(250,0,30,30);
-        mult1.setSymbol(HackAudio::Diagram::Junction::Symbol::Multiply);
-        mult1.setBounds(300,-64,30,30);
-        delay1.setPlaceholder("Delay");
-        delay1.setBounds(400,-17,96,64);
-        fb_gain1.setPlaceholder("Gain");
-        fb_gain1.setBounds(200,-128,64,48);
-        fb_node1.setSymbol(HackAudio::Diagram::Junction::Symbol::None);
-        fb_node1.setBounds(550,8,15,15);
+        sum_1.setSymbol(HackAudio::Diagram::Junction::Symbol::Add);
+        sum_1.setBounds(250,0,30,30);
+        mult_1.setSymbol(HackAudio::Diagram::Junction::Symbol::Multiply);
+        mult_1.setBounds(300,-64,30,30);
+        delay_1.setPlaceholder("Delay");
+        delay_1.setBounds(400,-17,96,64);
+        fb_1_gain.setPlaceholder("Gain");
+        fb_1_gain.setBounds(200,-128,64,48);
+        fb_1_node.setSymbol(HackAudio::Diagram::Junction::Symbol::None);
+        fb_1_node.setBounds(550,8,15,15);
         
-        sum2.setSymbol(HackAudio::Diagram::Junction::Symbol::Add);
-        sum2.setBounds(500,-64,30,30);
-        mult2.setSymbol(HackAudio::Diagram::Junction::Symbol::Multiply);
-        mult2.setBounds(440,-125,30,30);
-        delay2.setPlaceholder("z^-1");
-        delay2.setBounds(400,-72,64,48);
+        sum_2.setSymbol(HackAudio::Diagram::Junction::Symbol::Add);
+        sum_2.setBounds(500,-64,30,30);
+        mult_2.setSymbol(HackAudio::Diagram::Junction::Symbol::Multiply);
+        mult_2.setBounds(440,-125,30,30);
+        delay_2.setPlaceholder("z^{-1}");
+        delay_2.setBounds(400,-72,64,48);
         fb_gain2.setPlaceholder("Gain");
         fb_gain2.setBounds(500,-160,48,36);
-        fb_node2.setSymbol(HackAudio::Diagram::Junction::Symbol::None);
-        fb_node2.setBounds(350,-56.5,15,15);
+        fb_2_node.setSymbol(HackAudio::Diagram::Junction::Symbol::None);
+        fb_2_node.setBounds(350,-56.5,15,15);
         
-        addDiagramInput(sum1);
+        addDiagramInput(sum_1);
 
-        connect(sum1,delay1,HackAudio::Diagram::Junction::Horizontal);
-        connect(delay1, fb_node1);
-        connect(fb_node1,sum2);
-        connect(fb_gain1,mult1);
-        connect(sum2,delay2);
-        connect(delay2,mult1);
-        connect(fb_node2,mult2);
-        connect(fb_gain2,mult2);
-        connect(mult2,sum2,HackAudio::Diagram::Junction::Horizontal);
-        connect(mult1,sum1,HackAudio::Diagram::Junction::Horizontal);
+        connect(sum_1,delay_1,HackAudio::Diagram::Junction::Horizontal);
+        connect(delay_1, fb_1_node);
+        connect(fb_1_node,sum_2);
+        connect(fb_1_gain,mult_1);
+        connect(sum_2,delay_2);
+        connect(delay_2,mult_1);
+        connect(fb_2_node,mult_2);
+        connect(fb_gain2,mult_2);
+        connect(mult_2,sum_2,HackAudio::Diagram::Junction::Horizontal);
+        connect(mult_1,sum_1,HackAudio::Diagram::Junction::Horizontal);
         
-        addDiagramOutput(fb_node1);
+        addDiagramOutput(fb_1_node);
 
         setName("Low-Pass, Feed-Back Comb Filter");
 
